872cad614bfc91b21ed51e9dbf46d6329561e205
yanll <yanll@9a2970d3-d304-aaeb-c291-5cd4187d0b2d>
2012-06-18 19:21:15 +0000

    * Initial import projects. Basic framework is established, and Some simple functions are implemented.

---
+++ trunk/tests/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# This is a demo bash script for using pipeline directly.
+#
+
+. ../default.pipe
+
+REFERENCE=chrM.fa
+ALGORITHM=is
+INPUT_1=1.fq.gz
+INPUT_2=2.fq.gz
+OUTPUT=bwa_mapping.bam
+MAX_INSERT_SIZE=200
+
+SP_run bwa_reads_mapping $@
