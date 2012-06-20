e7918d8131a526bcaf6168b1af950ef6907bee9f
yanll <yanll@9a2970d3-d304-aaeb-c291-5cd4187d0b2d>
2012-06-20 01:44:09 +0000

    * Add input and output files dependency checking. * Use pipe to improve bash shell calling. * Rename 'pipeline' to 'procedure', and add workflow (input and output) attributes to every procedure.

--- tests/test.sh
+++ tests/test.sh
@@ -13,3 +13,20 @@ OUTPUT=bwa_mapping.bam
 MAX_INSERT_SIZE=200
 
 SP_run bwa_reads_mapping $@
+
+
+function bwa_reads_mapping
+{
+	SP_set QUAL_OPTION "-I" "${QUALITY_FORMAT_BASE}==64"
+	SP_run bwa_build_index REFERENCE=${REFERENCE}
+	SP_Parallel_Begin 2
+	bwa aln -t ${THREAD_NUM} -i ${BWA_END_IND} -e ${BWA_GAP_EXT} \
+		${REFERENCE} ${INPUT_2} -f ${INPUT_1}.sai ${QUAL_OPTION} &
+	bwa aln -t ${THREAD_NUM} -i ${BWA_END_IND} -e ${BWA_GAP_EXT} \
+		${REFERENCE} ${INPUT_2} -f ${INPUT_2}.sai ${QUAL_OPTION} &
+	SP_Parallep_End
+	bwa sampe -P ${REFERENCE} -a ${MAX_INSERT_SIZE} \
+		${INPUT_2}.sai ${INPUT_2}.sai ${INPUT_1} ${INPUT_2} \
+		| samtools view -Sb - \
+		> ${OUTPUT}
+}
