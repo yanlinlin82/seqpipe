147566291fa9cd15a44e8db001975dec3a9831db
Linlin Yan <yanlinlin82@gmail.com>
2017-02-01 21:13:31 +0800

    Update test script

--- tests/001_quick_start.pl
+++ tests/001_quick_start.pl
@@ -22,6 +22,12 @@ sub test_001
 	die if $lines[2] !~ /^\(1\) starts at $REGEX_TIME$/;
 	die if $lines[3] !~ /^\(1\) ends at $REGEX_TIME $REGEX_ELAPSE$/;
 	die if $lines[4] !~ /^$REGEX_UNIQUE_ID Pipeline finished successfully! $REGEX_ELAPSE$/;
+
+	die if `cat .seqpipe/last/log` ne $output;
+	die if `cat .seqpipe/last/pipeline` ne "echo 'Hello, world!'\n";
+	die if `cat .seqpipe/last/1.echo.cmd` ne "echo 'Hello, world!'\n";
+	die if `cat .seqpipe/last/1.echo.log` ne "Hello, world!\n";
+	die if `cat .seqpipe/last/1.echo.err` ne "";
 }
 test_001;
 
@@ -32,7 +38,6 @@ sub test_002
 	# prepare input
 	open my $fh, ">hello.pipe" or die;
 	print $fh 'echo "Hello, world!"
-sleep 1
 echo "Goodbye!"
 ';
 	close $fh;
@@ -42,18 +47,24 @@ echo "Goodbye!"
 
 	# check results
 	my @lines = split("\n", $output);
-	die if scalar @lines != 11;
+	die if scalar @lines != 8;
 	die if $lines[0] !~ /^$REGEX_UNIQUE_ID seqpipe run hello.pipe$/;
 	die if $lines[1] !~ /^\(1\) \[shell\] echo 'Hello, world!'$/;
 	die if $lines[2] !~ /^\(1\) starts at $REGEX_TIME$/;
 	die if $lines[3] !~ /^\(1\) ends at $REGEX_TIME $REGEX_ELAPSE$/;
-	die if $lines[4] !~ /^\(2\) \[shell\] sleep 1$/;
+	die if $lines[4] !~ /^\(2\) \[shell\] echo 'Goodbye!'$/;
 	die if $lines[5] !~ /^\(2\) starts at $REGEX_TIME$/;
 	die if $lines[6] !~ /^\(2\) ends at $REGEX_TIME $REGEX_ELAPSE$/;
-	die if $lines[7] !~ /^\(3\) \[shell\] echo 'Goodbye!'$/;
-	die if $lines[8] !~ /^\(3\) starts at $REGEX_TIME$/;
-	die if $lines[9] !~ /^\(3\) ends at $REGEX_TIME $REGEX_ELAPSE$/;
-	die if $lines[10] !~ /^$REGEX_UNIQUE_ID Pipeline finished successfully! $REGEX_ELAPSE$/;
+	die if $lines[7] !~ /^$REGEX_UNIQUE_ID Pipeline finished successfully! $REGEX_ELAPSE$/;
+
+	die if `cat .seqpipe/last/log` ne $output;
+	die if `cat .seqpipe/last/pipeline` ne "{\n\techo 'Hello, world!'\n\techo 'Goodbye!'\n}\n";
+	die if `cat .seqpipe/last/1.echo.cmd` ne "echo 'Hello, world!'\n";
+	die if `cat .seqpipe/last/1.echo.log` ne "Hello, world!\n";
+	die if `cat .seqpipe/last/1.echo.err` ne "";
+	die if `cat .seqpipe/last/2.echo.cmd` ne "echo 'Goodbye!'\n";
+	die if `cat .seqpipe/last/2.echo.log` ne "Goodbye!\n";
+	die if `cat .seqpipe/last/2.echo.err` ne "";
 
 	# clean up
 	unlink "hello.pipe";
