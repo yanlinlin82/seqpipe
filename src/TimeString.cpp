bb77346ba13012f43771f898ea60334eb07e3492
Linlin Yan <yanlinlin82@gmail.com>
2019-01-02 22:11:49 +0800

    Solve warning of 'format-truncation'

--- src/TimeString.cpp
+++ src/TimeString.cpp
@@ -4,7 +4,7 @@ std::string TimeString(time_t t)
 {
 	tm buf;
 	localtime_r(&t, &buf);
-	char text[32] = "";
+	char text[128] = "";
 	snprintf(text, sizeof(text), "%04d-%02d-%02d %02d:%02d:%02d",
 			buf.tm_year + 1900, buf.tm_mon + 1, buf.tm_mday,
 			buf.tm_hour, buf.tm_min, buf.tm_sec);
